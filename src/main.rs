85a906ff8ee1ce67566bef8e464aa02d67e6ea6c
Tomasz Różański <tomasz.rozanski@fastmail.com>
2020-03-11 15:09:44 +0100

    Refine the starter code
    - use static variable `F_CPU` to represent clock frequency,
    - replace deprecated method `NVIC::enable` with `NVIC::unmask`,
    - fix the timing issue, by using `SystClkSource::External` in the `syst.set_clock_source` method, to properly take advantage of 8 MHz onboard crystal,
    - substract 1 from the SysTick reload register (`SYST_RVR`) value (as one should),

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,21 @@ use cortex_m_rt::entry;
 use cortex_m_semihosting::hprint;
 use stm32f3::stm32f303::{interrupt, Interrupt, NVIC};
 
+static F_CPU: u32 = 8_000_000;
+
 #[entry]
 fn main() -> ! {
     let p = cortex_m::Peripherals::take().unwrap();
 
     let mut syst = p.SYST;
-    let mut nvic = p.NVIC;
 
-    nvic.enable(Interrupt::EXTI0);
+    unsafe {
+        NVIC::unmask(Interrupt::EXTI0);
+    }
 
     // configure the system timer to wrap around every second
-    syst.set_clock_source(SystClkSource::Core);
-    syst.set_reload(8_000_000); // 1s
+    syst.set_clock_source(SystClkSource::External);
+    syst.set_reload(F_CPU - 1); // 1s
     syst.enable_counter();
 
     loop {
